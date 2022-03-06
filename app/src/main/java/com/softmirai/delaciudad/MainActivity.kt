5da3c4403949716f1cc94433e0917837d225e567
lean alvarez <leanalvarez.dev@gmail.com>
2022-03-06 14:57:10 -0300

    navegacion y bottom navigation

--- app/src/main/java/com/softmirai/delaciudad/MainActivity.kt
+++ app/src/main/java/com/softmirai/delaciudad/MainActivity.kt
@@ -2,10 +2,31 @@ package com.softmirai.delaciudad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setActionBar(findViewById(R.id.toolBarMain))
+       // configNav()
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navBottomView: BottomNavigationView = findViewById(R.id.bnvMenu)
+        navBottomView.setupWithNavController(navController)
+
+
     }
+
+/*
+    fun configNav(){
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.
+        fragContent))
+    }
+    */
+
 }
